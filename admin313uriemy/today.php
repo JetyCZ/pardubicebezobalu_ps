7e92b74b24174aed76843d4f3ca0d50fc737ec44
Pavel Jetensky <pavel.jetensky@hooyu.com>
2022-01-03 16:40:37 +0100

    Calculation of dluh for whole year

--- admin313uriemy/today.php
+++ admin313uriemy/today.php
@@ -16,22 +16,30 @@ try {
     require_once _PS_ROOT_DIR_ . '/classes/custom/CustomUtils.php';
     require_once _PS_ROOT_DIR_ . '/classes/jety/Cron/CronExpression.php';
     CustomUtils::connect_to_database();
-if (isset($_REQUEST['d'])) {
-    $dateFilter = "date('".$_REQUEST['d']."')";
-} else {
-    $dateFilter = "now()";
-}
+
     $sql = <<<'EOD'
 select sum(op.amount) AS trzba_sum
 from ps_orders o       
         join ps_order_payment op on o.reference = op.order_reference
         join ps_order_state_lang osl on (osl.id_order_state = o.current_state and osl.id_lang=2) 
 where (              
-       (osl.name = 'Dodáno') and
-       date(o.date_add) = now() and     
+       (osl.name = 'OSL_NAME') and
+       DATE_ADD_FILTER and     
        1 = 1
        )
 EOD;
+
+    if (isset($_REQUEST['d'])) {
+        $sql = str_replace("OSL_NAME", 'Dodáno', $sql);
+        $dateFilter = "date('".$_REQUEST['d']."')";
+        $sql = str_replace("DATE_ADD_FILTER", 'date(o.date_add) = '.$dateFilter, $sql);
+    } else {
+        $sql = str_replace("OSL_NAME", 'Zaplaceno snížením dluhu Pavlovi', $sql);
+        $yearFilter = $_REQUEST['y'];
+        $sql = str_replace("DATE_ADD_FILTER", 'year(o.date_add) = '.$yearFilter, $sql);
+    }
+
+
     $sql = str_replace("now()", $dateFilter, $sql);
 //    order by sum(`od`.`total_price_tax_incl`) desc, `p`.`id_product`
     $result = $conn->query($sql);
